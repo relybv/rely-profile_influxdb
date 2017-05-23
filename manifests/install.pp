aefd67ea7ee36747c3d2adc3be566b89f2f55c56
P. Gomersbach <p.gomersbach@rely.nl>
2017-05-23 19:58:42 +0000

    use ensure_resorce for repo

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,6 +8,14 @@ class profile_influxdb::install {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
+  Apt::Pin <| |> -> Package <| |>
+  Apt::Source <| |> -> Package <| |>
+
+  $_operatingsystem = downcase($::operatingsystem)
+
+  ensure_resource('apt::source', 'influxrepo', {'ensure' => 'present', 'location' => "https://repos.influxdata.com/${_operatingsystem}", 'release' => "$::lsbdistcodename", 'repos' => 'stable', 'key' => { 'id' => '05CE15085FC09D18E99EFB22684A14CF2582E0C5', 'source' => 'https://repos.influxdata.com/influxdb.key',} })
+
+
   $global_config = {
     'bind-address'       => ':8088',
     'reporting-disabled' => false,
